6e9c21e69d0b03b781e36e104cf0a4b22d414e1d
Marcin Czernecki <marcin.cze@hotmail.com>
2017-08-15 17:29:09 +0200

    changing icons

--- Arrived/Resources/Resource.Designer.cs
+++ Arrived/Resources/Resource.Designer.cs
@@ -50,6 +50,9 @@ namespace Arrived
 			// aapt resource value: 0x7f020001
 			public const int Icon = 2130837505;
 			
+			// aapt resource value: 0x7f020002
+			public const int Message = 2130837506;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
